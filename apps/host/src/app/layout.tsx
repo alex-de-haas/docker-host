fd14e098e753eea2b6edd5356622eb33db1b91a6
Alex de Haas <a.zayats@outlook.com>
2026-05-21 17:09:16 +0200

    Add DH favicon to host app

--- apps/host/src/app/layout.tsx
+++ apps/host/src/app/layout.tsx
@@ -4,6 +4,10 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Docker Host Manager",
   description: "Manage installed Docker Host modules",
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
